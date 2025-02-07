56bae9fa8ebe0e266002e7046ba7821072ff571b
jjlindsey <jlindsey010@gmail.com>
2025-02-07 11:54:19 -0800

    update visual with world map and darkmode style

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import GeoRiskMap from './charts/GeoRiskMap'
 import Grid from '@mui/material/Grid2'
+import { Box } from '@mui/material'
+import ThemeToggle from './ThemeToggle'
 
 export default function Dashboard({data}) {
   return (
     <Grid>
+        <Box>
         Dashboard
+            <ThemeToggle />
+        </Box>
         <Grid item>
-            <GeoRiskMap neoData={data}/>
+            <GeoRiskMap neoData={data} />
         </Grid>
     </Grid>
   )
--- src/components/charts/GeoRiskMap.jsx
+++ src/components/charts/GeoRiskMap.jsx
@@ -1,24 +1,49 @@
 import React, { useEffect, useRef, useState } from 'react'
 import * as d3 from 'd3'
+import * as topojson from 'topojson-client'
 import { Box, Card, CardContent, Paper, Typography } from '@mui/material'
 
-export default function GeoRiskMap({ neoData = {}}) {
+export default function GeoRiskMap({ neoData = {}, darkMode}) {
     const svgRef = useRef(null)
-    //const [hoveredRegion, setHoveredRegion] = useState(null)
+    const [worldData, setWorldData] = useState(null)
 
     useEffect(() => {
-        if (!neoData || Object.keys(neoData).length === 0) return
+        d3.json('/countries-50m.json').then((topology) => {
+            const countries = topojson.feature(topology, topology.objects.countries)
+            setWorldData(countries)
+        })
+    }, [])
+
+    useEffect(() => {
+        if (! worldData || !neoData) return
 
         const width = 600
         const height = 300
 
+        const projection = d3.geoMercator()
+            .scale(130) //zoom level
+            .translate([width / 2, height / 1.5])
+
+        const pathGenerator = d3.geoPath(projection)
+
         const svg = d3.select(svgRef.current)
             .attr('width', width)
             .attr('height', height)
+            .style('background', darkMode ? '#1e1e1e' : '#eaf9fb')
             .style('border', '1px solid #ccc')
         
         //clear prev elements
         svg.selectAll('*').remove()
+
+        //draw countries
+        svg.selectAll('.country')
+            .data(worldData.features)
+            .enter()
+            .append('path')
+            .attr('class', 'country')
+            .attr('d', pathGenerator)
+            .attr('fill', darkMode ? '#2a2a2a' : '#ddd')
+            .attr('stroke', darkMode ? '#444' : '#aaa');
 
         //hazardous asteroid data
         const hazardousNeos = Object.values(neoData)
@@ -75,7 +100,7 @@ export default function GeoRiskMap({ neoData = {}}) {
                 d3.select('#tooltip').style('display', 'none')
             })
 
-    }, [neoData])
+    }, [worldData, neoData, darkMode])
 
   return (
     <Card>
@@ -85,7 +110,7 @@ export default function GeoRiskMap({ neoData = {}}) {
                 <svg ref={svgRef}></svg>
             </Box>  
             <Paper
-                    id="tooltip"
+                    id='tooltip'
                     sx={{
                         position: 'absolute',
                         display: 'none',
@@ -95,7 +120,7 @@ export default function GeoRiskMap({ neoData = {}}) {
                     }}
                 />
             <Box sx={{ mt: 2, display: 'flex', alignItems: 'center' }}>
-            <Typography variant="body2" sx={{ mr: 1 }}>Risk Level:</Typography>
+            <Typography variant='body2' sx={{ mr: 1 }}>Risk Level:</Typography>
                 <Box
                     sx={{
                     width: 200,
@@ -103,7 +128,7 @@ export default function GeoRiskMap({ neoData = {}}) {
                     background: 'linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,0.7))'
                     }}
                 />
-            <Typography variant="body2" sx={{ ml: 1 }}>High</Typography>
+            <Typography variant='body2' sx={{ ml: 1 }}>High</Typography>
             </Box>
         </CardContent>
     </Card>
