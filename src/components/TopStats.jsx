e1b83be853e3349b496f0697eeb83c247de16364
jjlindsey <jlindsey010@gmail.com>
2025-02-13 14:30:25 -0800

    add MUI datepicker to choose diff date range

--- src/components/TopStats.jsx
+++ src/components/TopStats.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import { useNeoStats } from '../hooks/useNeoStats'
-import { Grid2, Typography } from '@mui/material'
+import { Grid2, Typography, TextField } from '@mui/material'
 import StatisticCard from './StatisticCard'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { useNasa } from '../context/NasaContext'
+
+import dayjs from 'dayjs'
 
 
-export default function TopStats({ neoData}) {
+export default function TopStats({ neoData }) {
     const {totalCount, hazardousCount, nonHazardousCount} = useNeoStats(neoData)
+    const {startDate, endDate, setStartDate, setEndDate, fetchNeoData} = useNasa()
+
+    const startDayjs = dayjs(startDate)
+    //const endDayjs = dayjs(endDate)
+
+    const handleDateChange = (newValue) => {
+        if (!newValue) return
+        const newStartDate = newValue.format('YYYY-MM-DD')
+        const newEndDate = dayjs(newStartDate).add(7, 'day').format('YYYY-MM-DD')
+
+        setStartDate(newStartDate)
+        setEndDate(newEndDate)
+        fetchNeoData(newStartDate, newEndDate)
+    }
+
   return (
-    <Grid2 container spacing={1}>
-        <Typography>Date Range: </Typography>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Typography variant="h6">
+            Date Range: {startDate} to {endDate}
+        </Typography>
+        <Grid2 container spacing={1}>
+        <DatePicker
+            label='Start Date'
+            value={startDayjs}
+            onChange={handleDateChange}
+            renderInput={(params) => <TextField {...params} />}
+        />
         <Grid2 item xs={12} md={4}>
             <StatisticCard title='Total NEOs' value={totalCount} color='primary.main'/>
         </Grid2>
@@ -18,6 +47,7 @@ export default function TopStats({ neoData}) {
         <Grid2 item xs={12} md={4}>
             <StatisticCard title='Nonhazardous NEOs' value={nonHazardousCount}/>
         </Grid2>
-    </Grid2>
+        </Grid2>
+    </LocalizationProvider>
   )
 }
--- src/context/NasaContext.js
+++ src/context/NasaContext.js
@@ -15,11 +15,13 @@ export const NasaProvider = ({children}) => {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
     const [filteredData, setFilteredData] = useState(null)
+    const [startDate, setStartDate] = useState('2024-01-01')
+    const [endDate, setEndDate] = useState('2024-01-06')
     
     const API_KEY = process.env.REACT_APP_NASA_KEY
     const BASE_URL = 'https://api.nasa.gov/neo/rest/v1/feed'
 
-   const fetchNeoData = async (startDate, endDate) => {
+   const fetchNeoData = async (start, end) => {
     try {
         setLoading(true)
         setError(null)
@@ -33,6 +35,8 @@ export const NasaProvider = ({children}) => {
         const data = await response.json()
         setNeoData(data.near_earth_objects)
         setFilteredData(data.near_earth_objects)
+        setStartDate(start)
+        setEndDate(end)
     } catch (err) {
         setError(err.message)
         console.error('Error fetching NEO data:', err)
@@ -53,13 +57,11 @@ export const NasaProvider = ({children}) => {
     }
 
     useEffect(() => {
-        const startDate = '2024-01-01'
-        const endDate = '2024-01-06'
         fetchNeoData(startDate, endDate)
     }, [])
     
   return (
-    <NasaContext.Provider value={{ neoData: filteredData, loading, error, filterByHazard, fetchNeoData}}>
+    <NasaContext.Provider value={{ neoData: filteredData, loading, error, filterByHazard, fetchNeoData, startDate, endDate, setStartDate, setEndDate}}>
         {children}
     </NasaContext.Provider>
   )
